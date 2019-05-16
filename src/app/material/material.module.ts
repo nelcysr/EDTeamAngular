4c0cafab5680870b06d4e1281839bada1450d59e
Nelcy Sierra Rojas <nelcy.35.93@hotmail.com>
2019-05-16 16:40:08 -0500

    Agrega menú

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import {
+  MatToolbarModule, 
+  MatButtonModule,
+  MatIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [MaterialDemoComponent],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
 
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
