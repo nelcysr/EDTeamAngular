2a90d9d67647652449eedf7d14276bbe1f41232d
Nelcy Sierra Rojas <nelcy.35.93@hotmail.com>
2019-05-16 17:08:57 -0500

    Crea componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MaterialDemoComponent } from './material/material-demo/material-demo.component';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
 
   {
+    path:'',
+    component: AppComponent
+  },
+
+  {
     path: 'material-demo',
     component: MaterialDemoComponent 
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 
 ];
