c251a36ad88cf751b6395f3633ff2b6bc4a3f062
Nelcy Sierra Rojas <nelcy.35.93@hotmail.com>
2019-05-16 16:15:43 -0500

    Configuración Angular Material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialDemoComponent } from './material/material-demo/material-demo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'material-demo',
+    component: MaterialDemoComponent 
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialDemoComponent } from './material-demo/material-demo.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [MaterialDemoComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule
+  ],
+
+  exports: [
+    MatToolbarModule
+  ]
+})
+export class MaterialModule { }
