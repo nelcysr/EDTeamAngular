869129a8f61d3f29b80a8a1d77b5bfa74a829b8d
Nelcy Sierra Rojas <nelcy.35.93@hotmail.com>
2019-05-20 14:41:15 -0500

    Agrega interface e interpolación

--- src/app/material/material-list/material-list.component.ts
+++ src/app/material/material-list/material-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Estudiante } from 'src/app/shared/model/estudiante';
 
 @Component({
   selector: 'ed-material-list',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaterialListComponent implements OnInit {
 
+  estudiante1: Estudiante;
+  estudiante2: Estudiante;
+
   constructor() { }
 
   ngOnInit() {
+    console.log("Inicializando componente MaterialList");
+
+    this.estudiante1 = {
+      id: 1,
+      nombre: "Nelcy Sierra",
+      ciudad: "Bogotá",
+      fotoURL: "https://img.icons8.com/color/420/edit-user-female.png"
+    };
+
+
+    this.estudiante2 = {
+      id: 1,
+      nombre: "Pepito Pérez",
+      ciudad: "Cali",
+      fotoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI5TDjHkXbNGlduL7AEVMwGn4bc-QosNt7IRcp9fBJ4qN6N0pmHg"
+    };
+
+    setTimeout(() => {
+      this.estudiante2.ciudad = "Bucaramanga";
+
+      this.estudiante1 = {
+        
+        id: 7,
+        nombre: 'Deyanira',
+        ciudad: 'Leticia',
+        fotoURL: "https://img.icons8.com/color/420/edit-user-female.png"
+      }
+    }, 7000);
+    
   }
 
 }
